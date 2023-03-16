4778cd424e3c66b3e1447849154e21d13c5c8cec
Viktor Solovyev <barn-mih@rambler.ru>
2023-03-16 13:53:55 +0300

    feat: add header component and styled-components library

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./styles/normalize.css";
+import "./styles/global.css";
+
+// import { createGlobalStyle } from "styled-components";
+
+// const GlobalStyle = createGlobalStyle`
+// *,
+// *::before,
+// *::after {
+//   margin: 0;
+//   padding: 0;
+//   box-sizing: border-box;
+// }
+
+// body {
+//   min-height: 100vh;
+//   background: radial-gradient(yellow, orange);
+//   display: grid;
+//   place-content: center;
+//   text-align: center;
+// }
+// `;
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        {/* <GlobalStyle /> */}
+        <App />
+      </>
+    ),
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
