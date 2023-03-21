f2e4219d438aea92d3013f4854c5e6e0ff325df7
Viktor Solovyev <barn-mih@rambler.ru>
2023-03-21 14:46:13 +0300

    feat: add redirect to 404 route

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
 import "./styles/normalize.css";
 import "./styles/global.css";
 import AboutPage from "./pages/AboutPage";
@@ -21,8 +25,12 @@ const router = createBrowserRouter([
         element: <AboutPage />,
       },
       {
-        path: "*",
+        path: "404",
         element: <NotFoundPage />,
+      },
+      {
+        path: "*",
+        element: <Navigate to="404" />,
       },
     ],
   },
