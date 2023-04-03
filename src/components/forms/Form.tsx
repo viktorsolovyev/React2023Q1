42a7cc38df61d78d5c1ae0abe4c7d251299e1708
Viktor Solovyev <barn-mih@rambler.ru>
2023-04-04 02:10:50 +0300

    feat: add vaditation to the form

--- src/components/forms/Form.tsx
+++ src/components/forms/Form.tsx
@@ -1,140 +1,168 @@
-import React, { Component, MutableRefObject } from "react";
+import React, { FC, useEffect } from "react";
 import styled from "styled-components";
 import { TFormCard } from "types/types";
 import countries from "../../data/countries";
+import { useForm, SubmitHandler } from "react-hook-form";
 
-type Props = {
+enum GenderEnum {
+  female = "Female",
+  male = "Male",
+}
+
+type TFormValues = {
+  email: string;
+  birthday: string;
+  coutry: string;
+  consent: boolean;
+  gender: GenderEnum;
+  picture: FileList;
+};
+
+type FormProps = {
   addCardFunc: (card: TFormCard) => void;
 };
 
-type State = Record<string, never>;
+const Form: FC<FormProps> = ({ addCardFunc }) => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState,
+    formState: { errors, isSubmitSuccessful },
+  } = useForm<TFormValues>({ mode: "onSubmit", reValidateMode: "onSubmit" });
+  const onSubmit: SubmitHandler<TFormValues> = (data) => {
+    addCardFunc({
+      email: data.email,
+      birthday: new Date(data.birthday),
+      coutry: data.coutry,
+      consent: data.consent,
+      gender: data.gender,
+      picture: {
+        imageType: data.picture[0].type,
+        imageData: URL.createObjectURL(data.picture[0]),
+      },
+    });
+  };
 
-class Form extends Component<Props, State> {
-  inputEmail;
-  inputBirthday;
-  selectCoutry;
-  checkboxConsent;
-  radioGender: MutableRefObject<HTMLInputElement[] | null>;
-  inputPicture;
-
-  constructor(props: Props) {
-    super(props);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.clearFormData = this.clearFormData.bind(this);
-    this.inputEmail = React.createRef<HTMLInputElement>();
-    this.inputBirthday = React.createRef<HTMLInputElement>();
-    this.selectCoutry = React.createRef<HTMLSelectElement>();
-    this.checkboxConsent = React.createRef<HTMLInputElement>();
-    this.radioGender = React.createRef();
-    this.radioGender.current = [];
-    this.inputPicture = React.createRef<HTMLInputElement>();
-  }
-
-  handleSubmit(event: React.SyntheticEvent) {
-    event.preventDefault();
-    if (
-      this.inputEmail.current?.value &&
-      this.inputBirthday.current?.value &&
-      this.selectCoutry.current?.value &&
-      this.inputPicture.current &&
-      this.inputPicture.current.files &&
-      this.radioGender.current
-    ) {
-      const gender = this.radioGender.current.find(
-        (el) => el !== null && el.checked
-      )?.value;
-      this.props.addCardFunc({
-        email: this.inputEmail.current?.value,
-        birthday: new Date(this.inputBirthday.current?.value),
-        coutry: this.selectCoutry.current?.value,
-        consent: Boolean(this.checkboxConsent.current?.value),
-        gender: gender ? gender : "Male",
-        picture: {
-          imageType: this.inputPicture.current?.files[0].type,
-          imageData: URL.createObjectURL(this.inputPicture.current?.files[0]),
-        },
-      });
-      this.clearFormData();
+  useEffect(() => {
+    if (formState.isSubmitSuccessful) {
+      reset();
     }
-  }
+  }, [formState, isSubmitSuccessful, reset]);
 
-  clearFormData() {
-    if (
-      this.inputEmail.current?.value &&
-      this.inputBirthday.current?.value &&
-      this.selectCoutry.current?.value &&
-      this.checkboxConsent.current &&
-      this.inputPicture.current &&
-      this.inputPicture.current.files &&
-      this.radioGender.current
-    ) {
-      this.inputEmail.current.value = "";
-      this.inputBirthday.current.value = "";
-      this.selectCoutry.current.value = "";
-      this.checkboxConsent.current.checked = false;
-      this.inputPicture.current.value = "";
-      this.radioGender.current.forEach((item) => {
-        if (item !== null) item.checked = false;
-      });
-      this.radioGender.current = [];
-    }
-  }
-
-  render() {
-    return (
-      <StyledForm action="" onSubmit={this.handleSubmit}>
-        <div>
-          <h6>Email</h6>
-          <StyledInput ref={this.inputEmail} type="text" />
-        </div>
-        <div>
-          <h6>Birthday</h6>
-          <StyledInput ref={this.inputBirthday} type="date" />
-        </div>
-        <div>
-          <h6>Coutry</h6>
-          <StyledSelect defaultValue="" ref={this.selectCoutry}>
-            <option value="">----------</option>
-            {countries.map((country, index) => (
-              <option key={index} value={country.name}>
-                {country.label}
-              </option>
-            ))}
-          </StyledSelect>
-        </div>
-        <fieldset>
-          <legend>Select a gender:</legend>
-          <StyledRowFlexbox>
-            {["Male", "Female"].map((option) => (
-              <StyledLabel key={option}>
-                <StyledInput
-                  ref={(element: HTMLInputElement) =>
-                    this.radioGender.current?.push(element)
-                  }
-                  type="radio"
-                  name="gender"
-                  value={option}
-                />
-                <div>{option}</div>
-              </StyledLabel>
-            ))}
-          </StyledRowFlexbox>
-        </fieldset>
-        <div>
-          <h6>Profile picture</h6>
-          <StyledInput ref={this.inputPicture} type="file" />
-        </div>
-        <div>
-          <StyledLabel>
-            <StyledInput ref={this.checkboxConsent} type="checkbox" />
-            <div>I consent to my personal data</div>
-          </StyledLabel>
-        </div>
-        <StyledButton type="submit">Submit</StyledButton>
-      </StyledForm>
-    );
-  }
-}
+  return (
+    <StyledForm action="" onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <h6>Email</h6>
+        <StyledInput
+          {...register("email", {
+            required: "Email address is required",
+            pattern: {
+              value:
+                /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu,
+              message: "Email address is incorrect format",
+            },
+          })}
+          aria-invalid={errors.email ? "true" : "false"}
+          type="text"
+        />
+        {errors.email && (
+          <StyledP role="alert">{errors.email?.message}</StyledP>
+        )}
+      </div>
+      <div>
+        <h6>Birthday</h6>
+        <StyledInput
+          {...register("birthday", {
+            required: "Birthday is required",
+            validate: (value) =>
+              new Date(value) < new Date() ||
+              "Birthday can't be more than yesterday",
+          })}
+          type="date"
+          aria-invalid={errors.birthday ? "true" : "false"}
+        />
+        {errors.birthday && (
+          <StyledP role="alert">{errors.birthday?.message}</StyledP>
+        )}
+      </div>
+      <div>
+        <h6>Coutry</h6>
+        <StyledSelect
+          defaultValue=""
+          {...register("coutry", { required: "Coutry is required" })}
+          aria-invalid={errors.coutry ? "true" : "false"}
+        >
+          <option value="">----------</option>
+          {countries.map((country, index) => (
+            <option key={index} value={country.name}>
+              {country.label}
+            </option>
+          ))}
+        </StyledSelect>
+        {errors.coutry && (
+          <StyledP role="alert">{errors.coutry?.message}</StyledP>
+        )}
+      </div>
+      <fieldset>
+        <legend>Select a gender:</legend>
+        <StyledRowFlexbox>
+          {["Male", "Female"].map((option) => (
+            <StyledLabel key={option}>
+              <StyledInput
+                {...register("gender", { required: "Gender is required" })}
+                type="radio"
+                name="gender"
+                value={option}
+              />
+              <div>{option}</div>
+            </StyledLabel>
+          ))}
+        </StyledRowFlexbox>
+        {errors.gender && (
+          <StyledP role="alert">{errors.gender?.message}</StyledP>
+        )}
+      </fieldset>
+      <div>
+        <h6>Profile picture</h6>
+        <StyledInput
+          {...register("picture", {
+            required: "Profile picture is required",
+            validate: {
+              acceptedFormat: (files: FileList | null) =>
+                (files &&
+                  ["image/jpg", "image/png", "image/jpeg"].includes(
+                    files[0].type
+                  )) ||
+                "File is not an image",
+            },
+          })}
+          aria-invalid={errors.picture ? "true" : "false"}
+          type="file"
+        />
+        {errors.picture && (
+          <StyledP role="alert">{errors.picture?.message}</StyledP>
+        )}
+      </div>
+      <div>
+        <StyledLabel>
+          <StyledInput
+            {...register("consent", {
+              required: "You should consent to your personal data",
+            })}
+            type="checkbox"
+            aria-invalid={errors.consent ? "true" : "false"}
+          />
+          <div>I consent to my personal data</div>
+        </StyledLabel>
+        {errors.consent && (
+          <StyledP role="alert">{errors.consent?.message}</StyledP>
+        )}
+      </div>
+      <StyledButton type="submit">Submit</StyledButton>
+    </StyledForm>
+  );
+};
 
 const StyledRowFlexbox = styled.div`
   display: flex;
@@ -197,6 +225,11 @@ const StyledButton = styled.button`
     border: solid 1px #1c1c1c;
     background: white;
   }
+`;
+
+const StyledP = styled.p`
+  color: #ff4500;
+  font-size: 16px;
 `;
 
 export default Form;
