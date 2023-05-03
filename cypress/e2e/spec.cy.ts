b5993790c9ae40884d56058f4cd6fc7d6f9776bd
Viktor Solovyev <barn-mih@rambler.ru>
2023-05-03 19:14:51 +0300

    feat: add more some e2e tests

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -11,6 +11,20 @@ describe("The Home Page", () => {
     cy.contains("Rick Sanchez");
     cy.get('[data-testid="close-button"]').click();
   });
+  it("should menu links work", () => {
+    cy.visit("/");
+    cy.contains("About").click();
+    cy.get("h1").should("contain", "About");
+    cy.get("div").should("contain", "This is about page!");
+    cy.url().should("include", "/about");
+    cy.contains("Form").click();
+    cy.get("h1").should("contain", "Forms");
+    cy.url().should("include", "/forms");
+    cy.contains("Home").click();
+    cy.get("h1").should("contain", "Home");
+    cy.get('input[type="search"]').should("be.visible");
+  });
+
   it("Simple test", () => {
     expect(true).to.equal(true);
   });
